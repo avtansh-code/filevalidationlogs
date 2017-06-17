e3f1e37874af8b7c8daf92219d8eb1ae1cc0d93c
aashish347 <aashish.kumar@elucidata.io>
2017-06-17 10:46:06 +0530

    remove require statements for angular-ui-bootstrap and angular-sanitize

--- src/fvl_home_view/main.ts
+++ src/fvl_home_view/main.ts
@@ -3,8 +3,6 @@
 import registerAppDirective from './appViewDirective';
 export const AppName: string = 'fvl.home.view';
 
-const uibootstrap = require('angular-ui-bootstrap');
-const angularsanitize = require('angular-sanitize');
 
 const App = angular.module(AppName, ['ngMaterial', 
 				                    'ui.bootstrap',
